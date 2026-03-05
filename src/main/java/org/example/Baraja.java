e5317a82454ff419b63d675ba6e2e5d6512e7988
Sergio Mariño Estévez <sermaes2007@gmail.com>
2026-03-05 13:19:48 +0100

    segundo commit

--- src/main/java/org/example/Baraja.java
+++ src/main/java/org/example/Baraja.java
@@ -1,4 +1,29 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Baraja {
+    private List<Carta> cartas;
+
+    public Baraja() {
+        cartas = new ArrayList<>();
+
+        String[] palos = {"Corazones", "Diamantes", "Treboles", "Picas"};
+        String[] valores = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+        for (String palo : palos) {
+            for (String valor : valores) {
+                cartas.add(new Carta(palo, valor));
+            }
+        }
+    }
+
+    public void barajar(){
+        Collections.shuffle(cartas);
+    }
+
+    public Carta repartirCarta(){
+        return cartas.remove(0);
+    }
 }
--- src/main/java/org/example/Jugador.java
+++ src/main/java/org/example/Jugador.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class Jugador {
+    private String nombre;
+    private int creditos;
+    private Mano mano;
+
 }
