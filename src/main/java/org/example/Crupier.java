428279513d602eff3f0f6bcbeb200cb96705b047
Sergio Mariño Estévez <sermaes2007@gmail.com>
2026-03-10 13:48:30 +0100

    cuarto commit, clase crupier

--- src/main/java/org/example/Crupier.java
+++ src/main/java/org/example/Crupier.java
@@ -1,4 +1,29 @@
 package org.example;
 
 public class Crupier {
+    private Mano mano;
+
+    // Constructor
+    public Crupier() {
+        this.mano = new Mano();
+    }
+
+    // Getter de la mano
+    public Mano getMano() {
+        return mano;
+    }
+
+    // Lógica del turno del crupier
+    public void jugarTurno(Baraja baraja) {
+        // El crupier pide cartas mientras tenga menos de 17
+        while (mano.calcularValor() < 17) {
+            Carta carta = baraja.repartirCarta();
+            mano.agregarCarta(carta);
+        }
+    }
+
+    // Reinicia la mano para una nueva ronda
+    public void reiniciarMano() {
+        this.mano = new Mano();
+    }
 }
